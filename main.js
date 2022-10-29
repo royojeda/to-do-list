a78078e782b72483a48d22bfec2153ef3a9e7f46
Dennis Ojeda <royojeda1@gmail.com>
2022-10-29 23:49:47 +0800

    Add watch script

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-document.body.innerHTML='\n  <div class="underline bg-black text-white text-4xl">\n    tailwind test\n  </div>\n';
+document.body.innerHTML='\n  <div class="underline bg-black text-green-600 text-4xl">\n    tailwind test\n  </div>\n';
