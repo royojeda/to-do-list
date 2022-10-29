39a8a102113dea7c9b9d7408064cf7b96c94addb
Dennis Ojeda <royojeda1@gmail.com>
2022-10-30 00:12:10 +0800

    Add webpack dev server

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.body.innerHTML='\n  <div class="underline bg-black text-green-600 text-4xl">\n    tailwind test\n  </div>\n';
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.body.innerHTML = `
+  <div class="underline bg-black text-blue-600 text-4xl font-extralight">
+    tailwind test
+  </div>
+`
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LmJvZHkuaW5uZXJIVE1MID0gYFxuICA8ZGl2IGNsYXNzPVwidW5kZXJsaW5lIGJnLWJsYWNrIHRleHQtYmx1ZS02MDAgdGV4dC00eGwgZm9udC1leHRyYWxpZ2h0XCI+XG4gICAgdGFpbHdpbmQgdGVzdFxuICA8L2Rpdj5cbmBcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
