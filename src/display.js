cc81a4522cc11d54735b09d3cdac317c6fd358b1
Dennis Ojeda <royojeda1@gmail.com>
2022-11-15 20:58:26 +0800

    Add Display#layoutPage

--- src/display.js
+++ src/display.js
@@ -1,12 +1,26 @@
 export class Display {
-  constructor(
-    {
-      pageContainerSelector = '.container',
-      tasksContainerSelector = '.tasks',
-      projectsContainerSelector = '.projects'
-    } = {}) {
+  constructor(pageContainerSelector = '.page') {
     this.pageContainer = document.querySelector(pageContainerSelector);
-    this.tasksContainer = document.querySelector(tasksContainerSelector);
-    this.projectsContainer = document.querySelector(projectsContainerSelector);
+  }
+
+  layoutPage() {
+    this.pageContainer.className += ' ';
+    this.pageContainer.className += 'h-screen flex divide-x';
+    this.pageContainer.appendChild(this.createProjectsContainer());
+    this.pageContainer.appendChild(this.createTasksContainer());
+  }
+
+  createProjectsContainer() {
+    const component = document.createElement('div');
+    component.className = 'projects w-1/3 max-w-xs p-4';
+    component.textContent = 'projects';
+    return component;
+  }
+
+  createTasksContainer() {
+    const component = document.createElement('div');
+    component.className = 'tasks w-full p-4';
+    component.textContent = 'tasks';
+    return component;
   }
 }
--- src/todolist.js
+++ src/todolist.js
@@ -8,6 +8,6 @@ export class ToDoList {
   }
 
   setup() {
-
+    this.display.layoutPage();
   }
 }
